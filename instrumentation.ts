b82645c97cd3284f4ff8a387f2286f78b4e5ca29
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-07-16 15:21:54 -0500

    fix: simple instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,22 +1,7 @@
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
+import { registerOTel } from "@vercel/otel";
 
 export function register() {
   registerOTel({
     serviceName: "pokedex-next-app",
-    spanProcessors: [
-      new SimpleSpanProcessor(
-        new OTLPHttpJsonTraceExporter({
-          url: "https://agent-b0fd7500d1ab0740-bf80c4d36043094f.tracetest.io:443/v1/traces",
-        })
-      ),
-    ],
-    instrumentationConfig: {
-      fetch: {
-        propagateContextUrls: [
-          "https://agent-b0fd7500d1ab0740-bf80c4d36043094f.tracetest.io:443",
-        ],
-      },
-    },
   });
 }

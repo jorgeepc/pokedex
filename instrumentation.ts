627b7f7b40fd64d422aff817571f22a769596309
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2024-07-15 19:50:45 -0500

    test: add trace exporter

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,20 @@
-import { registerOTel } from "@vercel/otel";
+import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
+
+/* export function register() {
+  registerOTel({ serviceName: "pokedex-next-app" });
+} */
 
 export function register() {
-  registerOTel({ serviceName: "pokedex-next-app" });
+  registerOTel({
+    serviceName: "pokedex-next-app",
+    instrumentationConfig: {
+      fetch: {
+        propagateContextUrls: ["*"],
+      },
+    },
+
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: "https://agent-b0fd7500d1ab0740-bf80c4d36043094f.tracetest.io:443",
+    }),
+  });
 }
